d1e6e78b055130e687d2381795d705cdde0d0ac1
Troy Marrotte <Troy Marrotte>
2016-07-28 17:53:00 -0400

    testing

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
