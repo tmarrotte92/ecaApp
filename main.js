3488746b924355ce961b3f738144e9340456f291
Troy Marrotte <Troy Marrotte>
2016-07-28 18:56:54 -0400

    test

--- main.js
+++ main.js
@@ -21,11 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public/assets/')));
-app.use(express.static(path.join(__dirname, 'public/assets/css')));
-app.use(express.static(path.join(__dirname, 'public/assets/js')));
-app.use(express.static(path.join(__dirname, 'public/assets/img')));
+app.use('/static', express.static(__dirname + '/public'));
 
 app.use('/', routes);
 app.use('/users', users);
